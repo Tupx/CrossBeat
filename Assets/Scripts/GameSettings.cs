2f9ea694a499447619e8b73101943cde061dbe65
Christopher Bermudez <tupz0799@gmail.com>
2020-12-02 21:41:02 +0800

    Trigger Experiment

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -3,39 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSettings : MonoBehaviour
-{
-
-    public string fileData;
-    public string[] lines;
-    public string[] lineData;
-    public float x;
-    public float[] extractedData;
-
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public void PlayGame()
     {
-         extractedData = new float[128];
-         fileData = System.IO.File.ReadAllText("Assets/clearlySoundData.csv");
-         lines = fileData.Split("\n"[0]);
-
-        int a = 0;
-
-         foreach (string data in lines)
-        {
-            lineData = (lines[a].Trim()).Split(","[0]);
-            if (a <= 128)
-            {
-                extractedData[a] = float.Parse(lineData[0]);
-                Debug.Log(extractedData[a]);
-            }
-            a++;
-        }
+        Debug.Log("Clicked Play");
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void ExitGame()
     {
-        
+        Debug.Log("Exit Game");
+    }
+
+    public void  OpenSettings()
+    {
+        Debug.Log("Clicked Settings");
+        Application.Quit();
+    }
+
+    public void OpenAchievements()
+    {
+        Debug.Log("Clicked Achievements");
     }
 
 }
