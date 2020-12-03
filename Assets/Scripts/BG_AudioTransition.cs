76a0f8002bf3e84ecd4af657f2bf405a9b6db2ad
Christopher Bermudez <tupz0799@gmail.com>
2020-12-03 11:43:51 +0800

    Audio Animations

---
+++ Assets/Scripts/BG_AudioTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_AudioTransition : MonoBehaviour
+{
+
+    private static BG_AudioTransition instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
